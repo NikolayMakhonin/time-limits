23fcdd748485f3b887a604d23c6c59beb83abc40
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2025-10-29 10:08:00 +0700

    v1.0.3

--- src/pool/Pools.ts
+++ src/pool/Pools.ts
@@ -1,3 +1,188 @@
+// import {IAbortSignalFast} from '@flemist/abort-controller-fast'
+// import {
+//   Priority,
+//   IPriorityQueue,
+//   PriorityQueue,
+//   AwaitPriority,
+//   awaitPriorityDefault,
+// } from '@flemist/priority-queue'
+// import {isPromiseLike, promiseAll, promiseRace} from '@flemist/async-utils'
+// import {IPool} from './Pool'
+//
+// export class Pools implements IPool {
+//   private readonly _priorityQueue: IPriorityQueue
+//   private readonly _pools: IPool[]
+//
+//   constructor(...pools: IPool[]) {
+//     if (!pools?.length) {
+//       throw new Error('[Pools][release] pools should not be empty')
+//     }
+//     this._pools = pools
+//     this._priorityQueue = new PriorityQueue()
+//   }
+//
+//   get maxSize() {
+//     const pools = this._pools
+//     let min: number
+//     for (let i = 0, len = pools.length; i < len; i++) {
+//       const value = pools[i].maxSize
+//       if (i === 0 || value < min) {
+//         min = value
+//       }
+//     }
+//     return min
+//   }
+//
+//   get size() {
+//     const pools = this._pools
+//     let min: number
+//     for (let i = 0, len = pools.length; i < len; i++) {
+//       const value = pools[i].size
+//       if (i === 0 || value < min) {
+//         min = value
+//       }
+//     }
+//     return min
+//   }
+//
+//   get holdAvailable() {
+//     return this.size
+//   }
+//
+//   hold(
+//     count: number | number[],
+//   ): boolean {
+//     const size = this.size
+//     if (typeof count === 'number') {
+//       if (count > size) {
+//         return false
+//       }
+//     } else {
+//       for (let i = 0, len = count.length; i < len; i++) {
+//         if (count[i] > this._pools[i].size) {
+//           return false
+//         }
+//       }
+//     }
+//     const pools = this._pools
+//     for (let i = 0, len = pools.length; i < len; i++) {
+//       pools[i].hold(typeof count === 'number' ? count : count[i])
+//     }
+//     return true
+//   }
+//
+//   get releaseAvailable() {
+//     return this.maxSize - this.size
+//   }
+//
+//   release(
+//     count: number | number[],
+//     dontThrow?: boolean
+//   ): Promise<number> | number {
+//     const size = this.size
+//     const maxReleaseCount = this.maxSize - size
+//     if (count > maxReleaseCount) {
+//       if (dontThrow) {
+//         count = maxReleaseCount
+//       }
+//       else {
+//         throw new Error(`[Pools][release] count (${count} > maxReleaseCount (${maxReleaseCount}))`)
+//       }
+//     }
+//     if (count > 0) {
+//       const pools = this._pools
+//       let promises: Promise<number>[] = null
+//       for (let i = 0, len = pools.length; i < len; i++) {
+//         const promise = pools[i].release(count, dontThrow)
+//         if (isPromiseLike(promise)) {
+//           if (!promises) {
+//             promises = [promise]
+//           }
+//           else {
+//             promises.push(promise)
+//           }
+//         }
+//       }
+//       if (promises) {
+//         return promiseAll(promises).then(() => count)
+//       }
+//     }
+//     return count
+//   }
+//
+//   tick(abortSignal?: IAbortSignalFast): Promise<void> | void {
+//     let promises: Promise<void>[]
+//     for (let i = 0, len = this._pools.length; i < len; i++) {
+//       const promise = this._pools[i].tick(abortSignal)
+//       if (promise) {
+//         if (!promises) {
+//           promises = [promise]
+//         }
+//         else {
+//           promises.push(promise)
+//         }
+//       }
+//     }
+//
+//     if (!promises) {
+//       return null
+//     }
+//
+//     return promiseRace(promises)
+//   }
+//
+//   async holdWait(
+//     count: number | number[],
+//     priority?: Priority,
+//     abortSignal?: IAbortSignalFast,
+//     awaitPriority?: AwaitPriority,
+//   ) {
+//     if (typeof count === 'number') {
+//       if (count > this.maxSize) {
+//         throw new Error(`[Pools][holdWait] count (${count}) > maxSize (${this.maxSize}))`)
+//       }
+//     } else {
+//       for (let i = 0, len = count.length; i < len; i++) {
+//         if (count[i] > this._pools[i].maxSize) {
+//           throw new Error(`[Pools][holdWait] count[${i}] (${count[i]} > pools[i].maxSize (${this._pools[i].maxSize})`)
+//         }
+//       }
+//     }
+//
+//     if (!awaitPriority) {
+//       awaitPriority = awaitPriorityDefault
+//     }
+//
+//     await this._priorityQueue.run(async (abortSignal) => {
+//       while (true) {
+//         if (typeof count === 'number') {
+//           if (count <= this.size) {
+//             break
+//           }
+//         } else {
+//           let cahHold = true
+//           for (let i = 0, len = count.length; i < len; i++) {
+//             if (count[i] > this._pools[i].size) {
+//               cahHold = false
+//               break
+//             }
+//           }
+//           if (cahHold) {
+//             break
+//           }
+//         }
+//         await this.tick(abortSignal)
+//         if (awaitPriority) {
+//           await awaitPriority(priority, abortSignal)
+//         }
+//       }
+//       if (!this.hold(count)) {
+//         throw new Error('Unexpected behavior')
+//       }
+//     }, priority, abortSignal)
+//   }
+// }
+
 import {IAbortSignalFast} from '@flemist/abort-controller-fast'
 import {
   Priority,
@@ -49,6 +234,31 @@ export class Pools implements IPool {
     return this.size
   }
 
+  get releaseAvailable() {
+    return this.maxSize - this.size
+  }
+
+  tick(abortSignal?: IAbortSignalFast): Promise<void> | void {
+    let promises: Promise<void>[]
+    for (let i = 0, len = this._pools.length; i < len; i++) {
+      const promise = this._pools[i].tick(abortSignal)
+      if (promise) {
+        if (!promises) {
+          promises = [promise]
+        }
+        else {
+          promises.push(promise)
+        }
+      }
+    }
+
+    if (!promises) {
+      return null
+    }
+
+    return promiseRace(promises)
+  }
+
   hold(count: number): boolean {
     const size = this.size
     if (count > size) {
@@ -59,10 +269,6 @@ export class Pools implements IPool {
       pools[i].hold(count)
     }
     return true
-  }
-
-  get releaseAvailable() {
-    return this.maxSize - this.size
   }
 
   release(count: number, dontThrow?: boolean): Promise<number> | number {
@@ -95,27 +301,6 @@ export class Pools implements IPool {
       }
     }
     return count
-  }
-
-  tick(abortSignal?: IAbortSignalFast): Promise<void> | void {
-    let promises: Promise<void>[]
-    for (let i = 0, len = this._pools.length; i < len; i++) {
-      const promise = this._pools[i].tick(abortSignal)
-      if (promise) {
-        if (!promises) {
-          promises = [promise]
-        }
-        else {
-          promises.push(promise)
-        }
-      }
-    }
-
-    if (!promises) {
-      return null
-    }
-
-    return promiseRace(promises)
   }
 
   async holdWait(
